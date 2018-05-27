8d096700ecce0cfcff14b0014a5fd1f584d594e6
Gabriel <gabrielbonanimachado@gmail.com>
2018-05-26 23:16:46 -0300

    level5 is finalized

--- Python_Challenge/level5.py
+++ Python_Challenge/level5.py
@@ -8,4 +8,5 @@ information = urllib2.urlopen(page) #take the information as object
 
 data = pickle.load(information)#de-serielize the pickle object
 
-print data
+for line in data:
+    print("".join([k * v for k, v in line]))#join() will make two strings with k lines and v columms based on data information
